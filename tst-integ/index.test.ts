c6a5bc2fbc8368e3234dc5a73de4b6afac6f96b9
Jordan Ryan Moore <jordanryanmoore@me.com>
2020-09-23 10:21:32 -0700

    Fixing integration test.

--- tst-integ/index.test.ts
+++ tst-integ/index.test.ts
@@ -6,14 +6,14 @@ configDotEnv();
 test('MembersApi.findMember()', async () => {
     const email = process.env.SCOUT_EMAIL as string;
     const password = process.env.SCOUT_PASSWORD as string;
-    const authenticator = await new AuthenticatorFactory().create({
+    const authenticator = new AuthenticatorFactory().create({
         email,
         password,
     });
 
-    const memberId = authenticator.getPayload().id;
+    const memberId = await (authenticator.getPayload()).id;
     const membersApi = new MembersApi({
-        apiKey: (): string => authenticator.getToken(),
+        apiKey: (): Promise<string> => authenticator.getToken(),
     });
 
     expect((await membersApi.findMember(email)).data.member_id).toEqual(memberId);
